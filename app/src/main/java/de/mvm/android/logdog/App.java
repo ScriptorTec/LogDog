d6f716b0e047dc264bda6d0cab49f483557e67f8
Michael Woywod <michael_woywod@web.de>
2015-04-07 20:20:37 +0200

    changed App class and added missing line to Manifest

--- app/src/main/java/de/mvm/android/logdog/App.java
+++ app/src/main/java/de/mvm/android/logdog/App.java
@@ -4,25 +4,20 @@ import android.app.Application;
 
 public class App extends Application {
 
-    private static App instance = null;
-
-    private App() {
-        super();
-    }
+    private static App instance;
 
     /*
     * Singleton to get an instance of App
     * */
-    public static App getInstance() {
-        if(instance == null) {
-            instance = new App();
-        }
+    public static synchronized App getInstance() {
         return instance;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        instance = this;
     }
 
 }
